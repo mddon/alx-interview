9b36d4e5ad67956680b1dfa86f3eb1f4c912b06c
Miracle Obinna <mmrealworld@yahoo.com>
2024-03-06 14:51:39 -0800

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script on Pascal Triangle Mock Technical Interview"""
+"""Script on Pascal Triangle for Mock Technical Interview"""
 
 
 def pascal_triangle(n):
@@ -7,12 +7,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = []
+    pascal_triangle = []
     for i in range(n):
         row = [1] * (i + 1)
         if i > 1:
             for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
+                row[j] = pascal_triangle[i - 1][j - 1] + pascal_triangle[i - 1][j]
+        pascal_triangle.append(row)
 
-    return triangle
+    return pascal_triangle
