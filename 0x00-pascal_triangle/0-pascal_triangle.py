964da2eff7a3a30a24f6409690c356fbc3183b3e
Miracle Obinna <mmrealworld@yahoo.com>
2024-03-06 14:12:28 -0800

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script on Pascal Triangle Mock Technical Interview"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of numbers representing 
+    the Pascal triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,11 @@
+Pascal's Triangle
+Overview
+This Python function generates Pascal's triangle up to a specified number of rows. 
+
+Usage
+The function pascal_triangle(n) takes an integer n as input and returns a list of lists of integers representing Pascal's triangle with n rows.
+
+Parameters
+n (int): The number of rows in Pascal's triangle. Must be a positive integer.
+Returns
+A list of lists of integers representing Pascal's triangle with n rows.
