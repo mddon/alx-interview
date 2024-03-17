dc1154d84eaf78867464737ed76129f5bffe75cd
mddon <mmrealworld@yahoo.com>
2024-03-17 15:57:01 -0700

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""Defines a function that determines if a box containing a list
-   of lists can be opened using keys stored in the lists
+"""
+Defines a function that determines if a box containing a list
+of lists can be opened using keys stored in the lists
 """
 
 
-def canUnlockAll(boxes):
+def unlock(boxes):
     """Determines if boxes can be unlocked"""
-    position = 0
-    unlocked = {}
+    pos = 0
+    unlk = {}
 
-    for box in boxes:
-        if len(box) == 0 or position == 0:
-            unlocked[position] = "always_unlocked"
+    for idx, box in enumerate(boxes):
+        if len(box) == 0 or pos == 0:
+            unlk[pos] = "always_unlocked"
         for key in box:
-            if key < len(boxes) and key != position:
-                unlocked[key] = key
-        if len(unlocked) == len(boxes):
+            if key < len(boxes) and key != pos:
+                unlk[key] = key
+        if len(unlk) == len(boxes):
             return True
-        position += 1
+        pos += 1
     return False
