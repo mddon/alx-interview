7dae9d12d57a8ce0ed412bfc449ac650730b16ce
mddon <mmrealworld@yahoo.com>
2024-03-28 17:20:05 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,44 +2,53 @@
 
 import sys
 
-def print_msg(status_counts, total_size):
+
+def print_msg(dict_sc, total_file_size):
     """
-    Method to print the file size and status code metrics.
+    Method to print
     Args:
-        total_size (int): Total size of the files processed.
-        status_counts (dict): Dictionary containing status codes
+        dict_sc: dict of status codes
+        total_file_size: total of the file
     Returns:
-        None
+        Nothing
     """
 
-    print("File size: {}".format(total_size))
-    for key, code in sorted(status_counts.keys()):
-        if code != 0:
-            print("{}: {}".format(key, code))
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
-total_size = 0
-status_counts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-line_counter = 0
-program = 0
+
+total_file_size = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
     for line in sys.stdin:
-        parts = line.split()  # Splitting the input line into parts
-	parts = parts[::-1]
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
-        if len(parts) > 2:   # Checking if the line has exactly 9 parts
-            line_counter += 1
+        if len(parsed_line) > 2:
+            counter += 1
 
-		if line_counter <= 10:
-			total_size += int(parts[0])  # file size
-			program = parts[1]  # status code
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
 
-			if (program in status_counts.keys()):
-			status_counts[program] += 1
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
 
-		if line_counter == 10:  # Printing statistics every 10 lines
-            print_msg(status_counts, total_size)
-            line_counter = 0
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
 
 finally:
-    print_msg(status_counts, total_size)  # Printing final statistics
+    print_msg(dict_sc, total_file_size)
