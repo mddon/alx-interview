a211546b70ce5fa3e6f5a2fd117f4b9429de3422
mddon <mmrealworld@yahoo.com>
2024-04-04 11:56:41 -0700

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""UTF-8 Validation"""
+"""Script for UTF-8 Validation"""
 
 
 def validUTF8(data):
-    """Determines if a given data set
+    """Checks if a given data set
     represents a valid utf-8 encoding
     """
     number_bytes = 0
@@ -11,12 +11,11 @@ def validUTF8(data):
     mask_1 = 1 << 7
     mask_2 = 1 << 6
 
-    for i in data:
-
+    for byte in data:
         mask_byte = 1 << 7
 
         if number_bytes == 0:
-            while mask_byte & i:
+            while mask_byte & byte:
                 number_bytes += 1
                 mask_byte = mask_byte >> 1
 
@@ -24,9 +23,8 @@ def validUTF8(data):
                 continue
             if number_bytes == 1 or number_bytes > 4:
                 return False
-
         else:
-            if not (i & mask_1 and not (i & mask_2)):
+            if not (byte & mask_1 and not (byte & mask_2)):
                 return False
 
         number_bytes -= 1
