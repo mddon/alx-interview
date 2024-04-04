64fbbcafef6d5738cd295ede5cbffe7309fe62a3
mddon <mmrealworld@yahoo.com>
2024-04-04 11:44:22 -0700

    update

--- 0x04-utf8-validation/0-validate_utf8.py
+++ 0x04-utf8-validation/0-validate_utf8.py
@@ -3,38 +3,35 @@
 
 
 def validUTF8(data):
-        """Determines if a given data set
-            represents a valid utf-8 encoding
-                """
-                    number_bytes = 0
+    """Determines if a given data set
+    represents a valid utf-8 encoding
+    """
+    number_bytes = 0
 
-                        mask_1 = 1 << 7
-                            mask_2 = 1 << 6
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-                                for i in data:
+    for i in data:
 
-                                            mask_byte = 1 << 7
+        mask_byte = 1 << 7
 
-                                                    if number_bytes == 0:
+        if number_bytes == 0:
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
 
-                                                                    while mask_byte & i:
-                                                                                        number_bytes += 1
-                                                                                                        mask_byte = mask_byte >> 1
+            if number_bytes == 0:
+                continue
+            if number_bytes == 1 or number_bytes > 4:
+                return False
 
-                                                                                                                    if number_bytes == 0:
-                                                                                                                                        continue
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
 
-                                                                                                                                                if number_bytes == 1 or number_bytes > 4:
-                                                                                                                                                                    return False
+        number_bytes -= 1
 
-                                                                                                                                                                        else:
-                                                                                                                                                                                        if not (i & mask_1 and not (i & mask_2)):
-                                                                                                                                                                                                            return False
+    if number_bytes == 0:
+        return True
 
-                                                                                                                                                                                                                number_bytes -= 1
-
-                                                                                                                                                                                                                    if number_bytes == 0:
-                                                                                                                                                                                                                                return True
-
-                                                                                                                                                                                                                                return False
-
+    return False
